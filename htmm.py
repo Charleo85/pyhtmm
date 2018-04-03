35510e4e33a270c3a1ac8fc3eee5559a583ccb49
tonyqiu1019 <tq7bw@virginia.edu>
2018-04-03 18:08:06 -0400

    src to src translation part 2

--- htmm.py
+++ htmm.py
@@ -1,8 +1,50 @@
-import random, math
+import random, math, pickle
 import numpy as np
 
 
-class HTMM:
+"""
+Pickleable: An interface for loading and saving objects with pickle
+
+@Methods:
+- load(filepath): load this object from a file named "filepath"
+- save(filepath): save current state of object to file "filepath"
+"""
+
+class Pickleable:
+    def save(self, filepath):
+        data_file = open(filepath, 'wb')
+        pickle.dump(self, data_file)
+        data_file.close()
+
+    def load(self, filepath):
+        data_file = open(filepath, 'rb')
+        obj = pickle.load(data_file)
+        for attr, value in obj.__dict__.items():
+            setattr(self, attr, value)
+        data_file.close()
+
+
+"""
+HTMM: A Python implementation of the Hidden Topic Markov Model
+
+@Parameters:
+- topics: int
+- words: int
+- alpha: float
+- beta: float
+- iters: int
+- docs: list<Document>
+- epsilon: float
+- theta: numpy.ndarray(len(docs), topics)
+- phi: numpy.ndarray(topics, words)
+- p_dwzpsi: list<numpy.ndarray("# sentences in a doc", 2*topics)>
+- loglik: float
+
+@Methods:
+- inter(): train the model on the set of training documents (docs)
+"""
+
+class HTMM(Pickleable):
     def __init__(self, topics, words, alpha, beta, iters, fname, data_dir):
         self.topics_ = topics
         self.words_ = words
@@ -12,6 +54,7 @@ class HTMM:
         self.docs_ = []
         self.read_train_documents(fname, data_dir)
         self.rand_init_params()
+        self.loglik_ = 0.0
 
 
     def read_train_documents(self, fname, data_dir):
@@ -92,4 +135,43 @@ class HTMM:
 
 
     def find_epsilon(self):
-        
+        total = 0
+        lot = 0.0
+        for d in range(len(self.docs_)):
+            for i in range(self.docs_[d].num_sentences):
+                for z in range(self.topics_):
+                    lot += self.p_dwzpsi_[d][i][z]
+            total += self.docs_[d].num_sentences - 1
+        self.epsilon_ = lot / float(total)
+
+
+    def find_phi(self):
+        czw = np.zeros(self.topics_, self.words_)
+        self.count_topic_words(czw)
+
+        for z in range(self.topics_):
+            for w in range(self.words_):
+                self.phi_[z, w] = czw[z, w] + self.beta_ - 1
+            self.phi_[z] /= self.phi_[z].sum()
+
+
+    def count_topic_words(self, czw):
+        for d in range(len(self.docs_)):
+            for i in range(self.docs_[d].num_sentences):
+                sen = self.docs_[d].sentence_list[i]
+
+                for word in sen.word_list:
+                    for z in range(self.topics_):
+                        czw[z, w] += self.p_dwzpsi_[d][i][z] + self.p_dwzpsi_[d][i][z+self.topics_]
+
+
+    def find_theta(self):
+        for d in range(len(self.docs_)):
+            cdz = np.zeros(self.topics_)
+            for i in range(self.docs_[d].num_sentences):
+                for z in range(self.topics_):
+                    cdz[z] += self.p_dwzpsi_[d][i][z]
+
+            for z in range(self.topics_):
+                self.theta_[d, z] = cdz[z] + self.alpha_ - 1
+            self.theta_[d] /= self.theta_[d].sum()
